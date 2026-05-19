a2a43b6366a3a5a09ed3e417a5e2e5ddf0727a20
shiv5818 <shivkaushik5818@gmail.com>
2026-05-20 00:17:49 +0530

    adding essential files

---
+++ importantConcepts.js
@@ -0,0 +1,11 @@
+/*
+is URL encoding.In URLs, @ is a special separator character.
+Browsers, Node.js, MongoDB drivers all use this system.
+| Character | Encoding |
+| --------- | -------- |
+| `@`       | `%40`    |
+| `/`       | `%2F`    |
+| `:`       | `%3A`    |
+| `#`       | `%23`    |
+
+*/
